d92dc8809644618771913cbe8adfbe7c4f00b11f
Paulo Santos <paulo.santos@buser.com.br>
2022-12-15 18:53:38 -0300

    Cria link para página de ações individuais, cria template, cria view, e exibe no template poucas infos relevantes para uma ação

--- projeto/quotes/urls.py
+++ projeto/quotes/urls.py
@@ -4,4 +4,5 @@ from . import views
 urlpatterns = [
 	path('', views.home, name="home"),
 	path('all_stocks/', views.all_stocks, name="all_stocks"),
+	path('stock/', views.stock, name="stock"),
 ]
--- projeto/quotes/views.py
+++ projeto/quotes/views.py
@@ -11,8 +11,19 @@ def all_stocks(request):
 	api_request = requests.get("https://brapi.dev/api/quote/list")
 
 	try:
-		api = json.loads(api_request.content)["stocks"]
+		all_stocks = json.loads(api_request.content)["stocks"]
 	except Exception as e:
 		api = "Erro..."
 
-	return render(request, 'all_stocks.html', {'api':api})
+	return render(request, 'all_stocks.html', {'all_stocks':all_stocks})
+
+
+def stock(request):
+	api_request = requests.get("https://brapi.dev/api/quote/PETR4")
+
+	try:
+		stock = json.loads(api_request.content)["results"][0]
+	except Exception as e:
+		api = "Erro..."
+
+	return render(request, 'stock.html', {'stock':stock})
