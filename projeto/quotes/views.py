94ef69bfe4baab89a19a3438ff3bba34309bc1dc
Paulo Santos <paulo.santos@buser.com.br>
2022-12-15 23:18:34 -0300

    Mostra resultado de busca de qualquer ação com nome válido

--- projeto/quotes/views.py
+++ projeto/quotes/views.py
@@ -19,11 +19,13 @@ def all_stocks(request):
 
 
 def stock(request):
-	api_request = requests.get("https://brapi.dev/api/quote/PETR4")
-
-	try:
-		stock = json.loads(api_request.content)["results"][0]
-	except Exception as e:
-		api = "Erro..."
-
-	return render(request, 'stock.html', {'stock':stock})
+	if request.method == "POST":
+		stock_symbol = request.POST["stock_symbol"]
+		api_request = requests.get(f"https://brapi.dev/api/quote/{stock_symbol}")
+		try:
+			stock_info = json.loads(api_request.content)["results"][0]
+		except Exception as e:
+			stock_info = "Erro..."
+		return render(request, 'stock.html', {'stock':stock_info})
+	else:
+		return render(request, 'stock.html', {'stock_symbol': "coloca uma ação ai"})
