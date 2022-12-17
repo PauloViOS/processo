dd76193e028a4f6f55357f0fd673eb26a8b57dad
Paulo Santos <paulo.santos@buser.com.br>
2022-12-16 22:10:31 -0300

    Cria modelo HistoricalPrices e altera modelo Stock

--- projeto/quotes/models.py
+++ projeto/quotes/models.py
@@ -2,7 +2,13 @@ from django.db import models
 
 
 class Stock(models.Model):
-	ticker = models.CharField(max_length=10)
+	ticker = models.CharField(max_length=10, primary_key=True)
 
 	def __str__(self):
 		return self.ticker
+
+
+class HistoricalPrices(models.Model):
+	stock = models.ForeignKey(Stock, on_delete=models.CASCADE)
+	timestamp = models.IntegerField()
+	price = models.DecimalField(max_digits=10, decimal_places=2)
