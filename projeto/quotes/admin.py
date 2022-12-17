c322711a308a23c1d13504adcab74af414fd04e4
Paulo Santos <paulo.santos@buser.com.br>
2022-12-17 04:15:50 -0300

    Cria campo de noma da empresa da ação Muda nome de model para singular Constrói restrição em valores do modelo HistoricalPrice Registra HistoricalPrice no Admin

--- projeto/quotes/admin.py
+++ projeto/quotes/admin.py
@@ -1,4 +1,12 @@
 from django.contrib import admin
-from .models import Stock
+from .models import Stock, HistoricalPrice
 
-admin.site.register(Stock)
+
+class StockAdmin(admin.ModelAdmin):
+	list_display = ('ticker', 'company_name')
+
+class HistoricalPriceAdmin(admin.ModelAdmin):
+	list_display = ('stock', 'timestamp', 'price')
+
+admin.site.register(Stock, StockAdmin),
+admin.site.register(HistoricalPrice, HistoricalPriceAdmin)
--- projeto/quotes/models.py
+++ projeto/quotes/models.py
@@ -3,12 +3,18 @@ from django.db import models
 
 class Stock(models.Model):
 	ticker = models.CharField(max_length=10, primary_key=True)
+	company_name = models.CharField(max_length=40, null=True)
 
 	def __str__(self):
 		return self.ticker
 
 
-class HistoricalPrices(models.Model):
+class HistoricalPrice(models.Model):
 	stock = models.ForeignKey(Stock, on_delete=models.CASCADE)
 	timestamp = models.IntegerField()
 	price = models.DecimalField(max_digits=10, decimal_places=2)
+
+	class Meta:
+		constraints = [
+			models.UniqueConstraint(fields=['stock', 'timestamp'], name='dated_price')
+		]
