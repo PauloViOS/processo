708584c00276ec80e6090c74ecd5a5277c8d45ec
Paulo Santos <paulo.santos@buser.com.br>
2022-12-18 20:40:35 -0300

    adiciona url para login adicionar ação ao portfolio e remover ação do portfolio

--- projeto/projeto/urls.py
+++ projeto/projeto/urls.py
@@ -3,5 +3,6 @@ from django.urls import path, include
 
 urlpatterns = [
     path('', include('quotes.urls')),
+    path('', include("django.contrib.auth.urls")),
     path('admin/', admin.site.urls),
 ]
--- projeto/quotes/urls.py
+++ projeto/quotes/urls.py
@@ -5,6 +5,10 @@ urlpatterns = [
 	path('', views.home, name="home"),
 	path('all_stocks/', views.all_stocks, name="all_stocks"),
 	path('stock/', views.stock, name="stock"),
-	path('add_stock/', views.add_stock, name="add_stock"),
+	path('stock/<str:ticker>/', views.stock, name="stock_info"),
+	path('add_stock/<str:ticker>', views.add_stock, name="add_stock"),
+	path('remove_stock/<str:ticker>', views.remove_stock, name="remove_stock"),
 	path('remove_stock/<stock_id>', views.remove_stock, name="remove_stock"),
+	path('portfolio/', views.portfolio, name="portfolio"),
+	path('cadastro/', views.sign_up, name="cadastro"),
 ]
