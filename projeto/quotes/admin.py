a96d47161c16f0d3074e8093dcc17a6068cae44d
Paulo Santos <paulo.santos@buser.com.br>
2022-12-18 20:40:56 -0300

    mosgra infos de portfolio no admin

--- projeto/quotes/admin.py
+++ projeto/quotes/admin.py
@@ -1,12 +1,19 @@
 from django.contrib import admin
-from .models import Stock, HistoricalPrice
+from .models import Stock, HistoricalPrice, Portfolio
 
 
 class StockAdmin(admin.ModelAdmin):
-	list_display = ('ticker', 'company_name')
+	list_display = ('ticker', 'company_name', 'portfolio')
+
 
 class HistoricalPriceAdmin(admin.ModelAdmin):
 	list_display = ('stock', 'datetime', 'price')
 
+
+class PortfolioAdmin(admin.ModelAdmin):
+	list_display = ('name',)
+
+
 admin.site.register(Stock, StockAdmin),
 admin.site.register(HistoricalPrice, HistoricalPriceAdmin)
+admin.site.register(Portfolio, PortfolioAdmin),
